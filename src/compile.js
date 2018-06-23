486365edba5fc2e001d94f3bc49131e93a2c8d19
shiningWolf <31480234+shiningWolf@users.noreply.github.com>
2018-06-23 20:50:07 +0800

    修改更新回调函数

--- src/compile.js
+++ src/compile.js
@@ -25,8 +25,8 @@ class Compile{
 				//如果这个元素是{{}}这种格式
 				let key = RegExp.$1;
 				node.textContent =  this._vm[key];
-				new Watcher(this._vm,key,()=>{
-					node.textContent =  this._vm[key];
+				new Watcher(this._vm,key,val=>{
+					node.textContent =  val;
 				})
 			}
 		}else if(node.nodeType == 1){ //元素节点
@@ -38,13 +38,12 @@ class Compile{
 					node.addEventListener('input',()=>{
 						this._vm[attr.nodeValue] = node.value;	
 					})
-					new Watcher(this._vm,attr.nodeValue,()=>{
-						node.value =  this._vm[attr.nodeValue];
+					new Watcher(this._vm,attr.nodeValue,val=>{
+						node.value =  val;
 					})
 				}	
 
 			})	
 		}
-		
 	}
 }
